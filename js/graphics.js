fdbb5138fb7d09bca53c1ae00be5f1af148b3a01
Jaroslav Týc <mail@jaroslavtyc.com>
2017-06-04 18:13:46 +0200

    Attempt to fix background image jump on address bar toggle again

--- js/graphics.js
+++ js/graphics.js
@@ -1,3 +1,8 @@
 var background = document.getElementsByClassName('background')[0];
 
 background.style.height = window.height;
+
+window.addEventListener('resize', function () {
+    background.style.height = window.height;
+    console.log('zde');
+});
