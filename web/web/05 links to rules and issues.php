5b2e0ae20c5679976ef9b37acdaa1c7487d1dbcb
Jaroslav Týc <mail@jaroslavtyc.com>
2019-03-01 08:31:30 +0100

    Every link to rules uses trial pass

--- web/web/05 links to rules and issues.php
+++ web/web/05 links to rules and issues.php
@@ -3,7 +3,7 @@
 </div>
 <div class="row">
   <div class="col">
-    <a href="https://theurg.drdplus.info/#tabulka_formuli">Pravidla pro Theurga</a>
+    <a href="https://theurg.drdplus.info/?trial=1#tabulka_formuli">Pravidla pro Theurga</a>
   </div>
   <div class="col">
       <?= /** @var \DrdPlus\Calculators\Theurgist\FormulaWebPartsContainer $webPartsContainer */
