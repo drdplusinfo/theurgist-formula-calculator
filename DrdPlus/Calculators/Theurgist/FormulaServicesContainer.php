d4fa4768c1356563199bc19cc2012a2f40e9e323
Jaroslav Týc <mail@jaroslavtyc.com>
2019-10-16 17:41:34 +0200

    Updated underlying skeleton with changed menu icon

--- DrdPlus/Calculators/Theurgist/FormulaServicesContainer.php
+++ DrdPlus/Calculators/Theurgist/FormulaServicesContainer.php
@@ -5,6 +5,7 @@ namespace DrdPlus\Calculators\Theurgist;
 use DrdPlus\CalculatorSkeleton\CalculatorServicesContainer;
 use DrdPlus\RulesSkeleton\Dirs;
 use DrdPlus\RulesSkeleton\RoutedDirs;
+use DrdPlus\RulesSkeleton\Web\WebFiles;
 use DrdPlus\RulesSkeleton\Web\WebPartsContainer;
 use DrdPlus\Tables\Tables;
 
@@ -21,15 +22,28 @@ class FormulaServicesContainer extends CalculatorServicesContainer
     public function getRoutedWebPartsContainer(): WebPartsContainer
     {
         if ($this->formulaWebPartsContainer === null) {
-            $this->formulaWebPartsContainer = new FormulaWebPartsContainer(
-                $this->getPass(),
-                $this->getRoutedWebFiles(),
-                $this->getDirs(),
-                $this->getHtmlHelper(),
-                $this->getRequest(),
-                $this->getCurrentFormulaValues(),
-                $this->getTables()
-            );
+            $this->formulaWebPartsContainer = $this->createFormulaWebPartsContainer($this->getRoutedWebFiles());
+        }
+        return $this->formulaWebPartsContainer;
+    }
+
+    private function createFormulaWebPartsContainer(WebFiles $webFiles): FormulaWebPartsContainer
+    {
+        return new FormulaWebPartsContainer(
+            $this->getPass(),
+            $webFiles,
+            $this->getDirs(),
+            $this->getHtmlHelper(),
+            $this->getRequest(),
+            $this->getCurrentFormulaValues(),
+            $this->getTables()
+        );
+    }
+
+    public function getRootWebPartsContainer(): WebPartsContainer
+    {
+        if ($this->formulaWebPartsContainer === null) {
+            $this->formulaWebPartsContainer = $this->createFormulaWebPartsContainer($this->getRootWebFiles());
         }
         return $this->formulaWebPartsContainer;
     }
